f5892ef94eebaf435c8f87d8729eede485ae0e8f
suvo-oko <miravalles@gmail.com>
2021-05-26 10:40:54 +0200

    Add backend workflow

--- backend/tests/test_GetCounter.py
+++ backend/tests/test_GetCounter.py
@@ -2,11 +2,11 @@ import unittest
 
 import azure.functions as func
 
-from ..GetCounter import getNewCounterValue
+from ...backend.GetCounter import getNewCounterValue
 
+class TestCounter(unittest.TestCase):
+    def test_counter(self):
+        counter = 1
+        resp = getNewCounterValue(counter)
 
-def test_counter(self):
-    counter = 1
-    resp = getNewCounterValue(counter)
-
-    self.assertEqual(resp, 2)
+        self.assertEqual(resp, 2)
